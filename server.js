5c8705f88f70da8fff89a6083329c09539f39253
bonjurmrfirst <Illia_Potsepaiev@epam.com>
2017-06-21 16:02:50 +0300

    basic

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express();
-
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(8080);
 app.engine('html', require('ejs').renderFile);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.use(express.static('static/'));
+
 app.get('/', function (req, res) {
-  res.render('index.html', { pageCountMessage : null});
+  console.log('/');
+  res.render('index.html');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 app.listen(port, ip);
