dbfa6729aaecc8345caaf7644353e59356584726
bonjurmrfirst <Illia_Potsepaiev@epam.com>
2017-06-21 15:07:23 +0300

    ooke

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+console.log('Server running! on http://%s:%s', ip, port);
 
 module.exports = app;
